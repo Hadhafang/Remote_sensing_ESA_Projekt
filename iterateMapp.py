f01319a36801e5336d967e09eeae8046e034aae7
Hadhafang <ala09lbe@student.lu.se>
2017-01-23 14:27:50 +0100

    Added print statement for time evaluation

--- iterateMapp.py
+++ iterateMapp.py
@@ -2,15 +2,16 @@ import xml.etree.ElementTree as ET
 import os
 
 # os.chdir(r'C:\GISN24\Projekt')
-os.chdir(r'C:\GISN24')
+os.chdir(r'C:\GISN24') # Vi borde nog kopiera deras system
 print os.getcwd()
 
 # for root, dirs, files in os.walk("Z:\S2a\28PDC"):
 #     for name in files:
 #         if name.endswith('.xml'):
 #             print os.sep.join([dirpath, filename])
-
+iterations = 0
 for root, dirs, files in os.walk("C:\GISN24"):
+    iterations  += 1
     for name in files:
         if name.endswith('.xml'):
             print(os.path.join(root, name))
@@ -19,7 +20,7 @@ for root, dirs, files in os.walk("C:\GISN24"):
         if name.endswith('.xml'):
             print(os.path.join(root, name))
         # b.append(os.path.join(root,name))
-
+    print("there has been " + iterations +"iterations")
 # tree = ET.parse('MTD_MSIL1C.xml')
 # root = tree.getroot()
 #

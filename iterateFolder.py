0c76c8047fff985a131bd3b2c9ff17de50782fde
Hadhafang <ala09lbe@student.lu.se>
2017-02-09 12:56:27 +0100

    Added some info

--- iterateFolder.py
+++ iterateFolder.py
@@ -4,7 +4,7 @@ import os
 '''
 @author: manand83
 '''
-
+# Retrives the clod content in the MTD file in teh owerarching folder
 def extractClouds(dir, metadata):
     os.chdir(dir)
     print os.getcwd()
@@ -15,11 +15,14 @@ def extractClouds(dir, metadata):
     for Quality_Indicators_Info in root.iter('Cloud_Coverage_Assessment'):
         return Quality_Indicators_Info.text
 
-os.chdir(r'C:\GISN24')
-
-for root, dirs, files in os.walk("C:\GISN24"):
+# Retriving of the Mtd files        
+os.chdir(r'C:\Users\Leonard Berge\Downloads\Mans')
+iterations = 0 # Will count the number of iterations to find the meta data files
+for root, dirs, files in os.walk("C:\Users\Leonard Berge\Downloads\Mans"):
     for name in files:
+        iterations += 1
         if name.endswith('.xml'):
             if "MTD" in name:
-                print root
-                print extractClouds(root,name)
+                print "\n" + root
+                print "\n" + extractClouds(root,name)
+        print ("\n There has been:" + str(iterations) + "iterations")

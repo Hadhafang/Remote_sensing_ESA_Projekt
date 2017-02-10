307b5698e13637680323e5b833ef951dac0c742b
MikaelRyd <mikaelrydstrom@hotmail.com>
2017-02-10 11:31:47 +0100

    Commit

--- iterateFolder.py
+++ iterateFolder.py
@@ -4,37 +4,13 @@ import os
 '''
 @author: manand83
 '''
-<<<<<<< HEAD
-# Retrives the cloud and time stamp content in the MTD file in teh owerarching folder
-def extractCloudsTime(dir, metadata):
-=======
-#class extractInfo:
-    
+#class extractMTD:
+  #  def __init__():
+        
 def extractClouds(dir, metadata):
->>>>>>> refs/remotes/origin/MikaelBranch2
     os.chdir(dir)
     tree = ET.parse(metadata)
     root = tree.getroot()
-<<<<<<< HEAD
-    Quality_Indicators =''
-    
-    for Product_Info in root.iter('Product_Info'): # Finds product info
-        PST = Product_Info.find("PRODUCT_START_TIME").text #Start time
-        #Quality_Indicators += "\n Product start time" + PET
-        PET = Product_Info.find("PRODUCT_STOP_TIME").text #End time or stop time
-        GT = Product_Info.find("GENERATION_TIME").text   # Gin and Tonic
-        Quality_Indicators += "\n Product start time" + PST + "\n Product end time" + PET +"\n Generation time" + GT
-        print Quality_Indicators
-
-    for Quality_Indicators_Info in root.iter('Cloud_Coverage_Assessment'):
-        Quality_Indicators += "\n Cloud Coverage Assesment " + str(Quality_Indicators_Info.text)
-    return Quality_Indicators
-        
-# Retriving of the Mtd files        
-os.chdir(r'C:\Users\Leonard Berge\Downloads\Mans\T28PDC')
-iterations = 0 # Will count the number of iterations to find the meta data files
-for root, dirs, files in os.walk("C:\Users\Leonard Berge\Downloads\Mans\T28PDC"):
-=======
     result = ''
     for Product_Info in root.iter('Product_Info'): # Finds product info
         PI = Product_Info.find('PRODUCT_URI').text #Product name
@@ -65,18 +41,9 @@ def extractSunAngle(dir, metadata):
 os.chdir(r'C:\Mons')
 
 for root, dirs, files in os.walk("C:\Mons"):
->>>>>>> refs/remotes/origin/MikaelBranch2
     for name in files:
-        iterations += 1
-        if name.endswith('.xml'):
-            if "MTD" in name:
-<<<<<<< HEAD
-                print "\n" + root
-                print "\n" + extractCloudsTime(root,name)
-                break
-    print ("\n There has been:" + str(iterations) + "iterations")
-=======
-                print extractClouds(root,name)
-                print extractSunAngle(root,name)
+            if name.endswith('.xml'):
+               if "MTD" in name:
+                  print extractClouds(root,name)
+                  print extractSunAngle(root,name)
                 
->>>>>>> refs/remotes/origin/MikaelBranch2

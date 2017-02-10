9a3ccd2a9f74ff74297c332cc3cf16fd058a2e52
MikaelRyd <mikaelrydstrom@hotmail.com>
2017-02-10 11:17:53 +0100

    Product_URI etc

--- iterateFolder.py
+++ iterateFolder.py
@@ -8,16 +8,15 @@ import os
     
 def extractClouds(dir, metadata):
     os.chdir(dir)
-    print os.getcwd()
-    print metadata
     tree = ET.parse(metadata)
     root = tree.getroot()
     result = ''
     for Product_Info in root.iter('Product_Info'): # Finds product info
+        PI = Product_Info.find('PRODUCT_URI').text #Product name
         PST = Product_Info.find("PRODUCT_START_TIME").text #Start time
         PET = Product_Info.find("PRODUCT_STOP_TIME").text #End time or stop time
         GT = Product_Info.find("GENERATION_TIME").text   # Gin and Tonic
-        result += "\n Product start time" + PST + "\n Product end time" + PET +"\n Generation time" + GT
+        result += "\n Product Name: " +PI +  "\n Product start time: " + PST + "\n Product end time: " + PET +"\n Generation time: " + GT
        
 
     for Quality_Indicators_Info in root.iter('Cloud_Coverage_Assessment'):
@@ -44,7 +43,6 @@ for root, dirs, files in os.walk("C:\Mons"):
     for name in files:
         if name.endswith('.xml'):
             if "MTD" in name:
-                print root
                 print extractClouds(root,name)
                 print extractSunAngle(root,name)
                 

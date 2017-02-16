fd899a8be50855b8a1b572489f29cf9846279224
Hadhafang <ala09lbe@student.lu.se>
2017-02-16 16:47:32 +0100

    Började jobba med iterate folder

--- iterateFolder.py
+++ iterateFolder.py
@@ -3,13 +3,9 @@ import os
 import extractMTD
 
 '''
-@author: manand83
+@author: manand83, hadhafang
 '''
 
-        
-
-    
-    
 class iterateFolder:
     def __init__(self, startDir):
         self.dir = startDir
@@ -20,6 +16,9 @@ class iterateFolder:
             for name in files:
                 if name.endswith('.xml'):
                     if "MTD" in name:
-                        print extractClouds(root,name)
-                        print extractSunAngle(root,name)
+                        self.metadata.append.extractMTD() #it should only append witth the outhermost mtd
+                        self.metadata[-1].TimeCloud(root,name) #For the outermost mtd
+                        self.metadata[-1].SunAngle(root,name) #This will always be wrong now
+                        # print extractClouds(root,name)
+                        #print extractSunAngle(root,name)
                 

33e2967eaaf2be1fbe1d23c5981d7c81be06aa70
Hadhafang <ala09lbe@student.lu.se>
2017-02-17 11:10:18 +0100

    TileListFix

--- renderTilelist.py
+++ renderTilelist.py
@@ -6,20 +6,25 @@ import os
 @author: hadhafang
 render a list of all tiles and certain parts of their meta data.
 '''
-#dir = os.chdir(r'C:\Mons') #change it to the path as needed
-#file = iteratFolder(dir)
+dir = os.chdir(r'C:\Mons') #change it to the path as needed
+files = iteratFolder(dir)
 
 
-#open(SentinelList.txt,w) # Name of file. 'w' indicates write
+open(SentinelList.txt,w) # Name of file. 'w' indicates write
 now = datetime.datetime.now()
-#string buliding
+# string buliding or rather table building
 year = str(now.year)
 month =str (now.month)
 day = str(now.day)
 hour = str(now.hour)
 
 
-rT = year+'-'+month+'-'+day +' ' +hour+'\n' #Rendering time a time stamp
+rT = year + '-'+ month + '-' + day + ' ' + hour +'\n' #Rendering time a time stamp
 
 
 table = rT + 'id\tstart_time\tstop_time\tstart_equals_end\tgeneration_time\tCloud_Coverage_Assessment\tZenith_angel\tAzimuth_angel'
+for file in files: # iterates through the file list
+    table += '\n'
+    for data in file: # iterates through the list of metadata
+        table += str(data) +'\t'
+        

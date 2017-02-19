ab6f69ee5744174bc3bccbdf84dbdf7a88ef7b91
Hadhafang <ala09lbe@student.lu.se>
2017-02-19 11:17:21 +0100

    Tog bort någon gammal header

--- iterateFolder.py
+++ iterateFolder.py
@@ -5,44 +5,6 @@ import extractMTD
 '''
 @author: manand83, hadhafang
 '''
-<<<<<<< HEAD
-#class extractMTD:S
-  #  def __init__():
-        
-def extractClouds(dir, metadata):
-    os.chdir(dir)
-    tree = ET.parse(metadata)
-    root = tree.getroot()
-    result = ''
-    for Product_Info in root.iter('Product_Info'): # Finds product info
-        PI = Product_Info.find('PRODUCT_URI').text #Product name
-        PST = Product_Info.find("PRODUCT_START_TIME").text #Start time
-        PET = Product_Info.find("PRODUCT_STOP_TIME").text #End time or stop time
-        GT = Product_Info.find("GENERATION_TIME").text   # Gin and Tonic
-        result += "\n Product Name: " +PI +  "\n Product start time: " + PST + "\n Product end time: " + PET +"\n Generation time: " + GT
-       
-
-    for Quality_Indicators_Info in root.iter('Cloud_Coverage_Assessment'):
-        result+= Quality_Indicators_Info.text
-    
-    return result
-    
-def extractSunAngle(dir, metadata):
-    os.chdir(dir)
-    tree = ET.parse(metadata)
-    root = tree.getroot()
-    result =''
-    
-    for Mean_Sun_Angle in root.iter('Mean_Sun_Angle'):
-        result += '\n' + ('Mean Zenith Angle: ') + Mean_Sun_Angle.find('ZENITH_ANGLE').text
-        result += '\n' + ('Mean Azimuth Angle: ') + Mean_Sun_Angle.find('AZIMUTH_ANGLE').text
-    
-    return result
-    
-    
-os.chdir(r'C:\Mons')
-=======
->>>>>>> origin/master
 
 class iterateFolder:
     def __init__(self, startDir):

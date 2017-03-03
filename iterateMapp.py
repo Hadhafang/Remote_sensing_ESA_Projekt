905b1aec1cdaf484d694d5252918845552cded79
mara91 <m.andersson1991@gmail.com>
2017-03-03 22:41:45 +0100

    Export to .xlsx
    Adderad funktionalitet som extraherar metadata och exporterar i en
    Excel-fil.

--- iterateMapp.py
+++ iterateMapp.py
@@ -1,11 +1,16 @@
 import xml.etree.ElementTree as ET
 import os
+import xlsxwriter
 
 '''
 @author: manand83
 '''
 
-def walklevel(some_dir, level=10):
+# -------------------------------------------------------------
+# METHOD THAT DEFINES HOW DEEP THE ITERATION WILL GO, IF LEVEL
+# IS SET TO 2 THE ITERATION WILL GO 2 FOLDERS DEEP AND NOT FURHTER
+# -------------------------------------------------------------
+def walklevel(some_dir, level):
     some_dir = some_dir.rstrip(os.path.sep)
     assert os.path.isdir(some_dir)
     num_sep = some_dir.count(os.path.sep)
@@ -14,13 +19,99 @@ def walklevel(some_dir, level=10):
         num_sep_this = root.count(os.path.sep)
         if num_sep + level <= num_sep_this:
             del dirs[:]
-
+# -------------------------------------------------------------
+# DECLARE VECTORS TO STORE METADATA
+# -------------------------------------------------------------
+name = [] # name of the product (ex 28PDC)
+id = [] # id of the product
+cloud = [] #
+startTime = [] #the start time of the product
+stopTime = [] # the stop time of the product
+timeEqual = [] # boleean true if end time equals start time otherwise false
+genTime = [] # generation time
+zenithAngle = [] # zenith angle
+azimuthAngle = [] # azimuth angle
+# -------------------------------------------------------------
+# IMPORT METHODS TO EXTRACT METADATA FROM XML-DOCUMENT
+# -------------------------------------------------------------
 from extractMetadata import extractClouds
-os.chdir(r'C:\GISN24')
+from extractMetadata import extractId
+from extractMetadata import extractStartTime
+from extractMetadata import extractStopTime
+from extractMetadata import extractGenTime
+from extractMetadata import extractZenithAngle
+from extractMetadata import extractAzimuthAngle
 
-for root, dirs, files in walklevel('C:\GISN24'):
+# -------------------------------------------------------------
+# STORES THE NAME OF THE TOP FOLDER (ex 28PDC)
+# -------------------------------------------------------------
+os.chdir(r'C:\GISN24')
+for root, dirs, files in walklevel('C:\GISN24', 2):
+    name.append(os.path.abspath(os.path.join(root, os.pardir)))
+print name
+
+# -------------------------------------------------------------
+# ITERATION EXTRACTING INFORMATION FROM THE FIRST METADATA-FILE
+# -------------------------------------------------------------
+os.chdir(r'C:\GISN24')
+for root, dirs, files in walklevel('C:\GISN24', 2):
     for name in files:
         if name.endswith('.xml'):
             if "MTD" in name:
-                print root
-                print extractClouds(root,name)
+                cloud.append(extractClouds(root,name))
+                id.append(extractId(root,name))
+                startTime.append(extractStopTime(root,name))
+                stopTime.append(extractStopTime(root,name))
+                genTime.append(extractGenTime(root,name))
+
+# -------------------------------------------------------------
+# ITERATION EXTRACTING  INFORATION FROM THE GRANULE FOLDER
+# -------------------------------------------------------------
+for root, dirs, files in walklevel('C:\GISN24', 3):
+    for name in dirs:
+        if root.endswith('GRANULE'): # finds folder "GRANULE" and saves the path to "directory"
+            directory = root
+            for root, dirs, files in walklevel(directory, 3):
+                for name in files:
+                    if name.endswith('.xml'):
+                        if "MTD" in name:
+                            zenithAngle.append(extractZenithAngle(root,name))
+                            azimuthAngle.append(extractAzimuthAngle(root,name))
+
+# -------------------------------------------------------------
+# CHECK IF START TIME AND STOP TIME IS EQUAL
+# -------------------------------------------------------------
+for i,item in enumerate(startTime):
+     timeEqual.append(startTime[i] == stopTime[i]) #returns true if the time is equal
+
+
+# -------------------------------------------------------------
+# EXPORT METADATA TO EXCEL SHEET
+# -------------------------------------------------------------
+os.chdir(r'C:\GISN24') #Specifies where to save the file
+# Create a workbook and add a worksheet.
+workbook = xlsxwriter.Workbook('sentinel123.xlsx')
+worksheet = workbook.add_worksheet()
+
+# Sets heading to columns
+worksheet.write(0, 1, 'Id') # (row, col, data)
+worksheet.write(0, 2, 'Clouds')
+worksheet.write(0, 3, 'Start time')
+worksheet.write(0, 4, 'Stop time')
+worksheet.write(0, 5, 'Is equal')
+worksheet.write(0, 6, 'Generation time')
+worksheet.write(0, 7, 'Zenith angle')
+worksheet.write(0, 8, 'Asimuth angle')
+
+# Walks through every list and puts the information in correct cell
+for index, member in enumerate(id):
+     worksheet.write(index+1, 1,id[index])
+     worksheet.write(index+1, 2,cloud[index])
+     worksheet.write(index+1, 3,startTime[index])
+     worksheet.write(index+1, 4,stopTime[index])
+     worksheet.write(index+1, 5,timeEqual[index])
+     worksheet.write(index+1, 6,genTime[index])
+     worksheet.write(index+1, 7,zenithAngle[index])
+     worksheet.write(index+1, 8,azimuthAngle[index])
+
+workbook.close()

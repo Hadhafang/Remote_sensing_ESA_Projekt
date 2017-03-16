ab4b0248b5faf6aa428794aa1abad4c1d9dc9447
MikaelRyd <mikaelrydstrom@hotmail.com>
2017-03-16 10:38:14 +0100

    sdw
    d

--- tileFinder.py
+++ tileFinder.py
@@ -1,7 +1,34 @@
 import xml.etree.ElementTree as ET
 import os
+import xlrd
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+#tilename tabellnamn.  x-axel -> start/stop time.  y-axel cloud
 '''
-@author: hadhafang
-Finds the tile linked to a coordinate or a tilename
+@author: Mikael
+Plots 
 '''
+
+def test(dir,filename):
+   os.chdir(dir)
+   wb = xlrd.open_workbook(filename)
+   first_sheet = wb.sheet_by_index(0)
+   xticks = []
+   for row in range(first_sheet.nrows):
+        xticks.append(first_sheet.cell_value(row+1,3))
+   for row in range(first_sheet.nrows):
+       x = [row]
+      
+       cld = first_sheet.cell_value(row+1,2)
+       plt.title(first_sheet.cell_value(row+1,0))
+       plt.xticks(x,xticks,rotation=90)
+       plt.plot(cld,x,'*')
+       
+       plt.show()
+      
+       
+       
+test('C:\Users\Mikael\Downloads','sentinel123.xlsx')
+       

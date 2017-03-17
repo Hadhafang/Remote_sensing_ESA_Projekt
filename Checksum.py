cd943724d59b7d2f9320b4a911a6b89e2c578d66
Hadhafang <ala09lbe@student.lu.se>
2017-03-17 14:55:32 +0100

    Mer med  checksum och nyttigheter
    I nyttigheter ligger ett exempel på hur md5 ska se ut.

--- Checksum.py
+++ Checksum.py
@@ -4,12 +4,25 @@ import os, hashlib
 Created on Thu Mar 16 12:04:52 2017
 
 @author: Hadhafang
+Will produce MD5 of root folder
 """
 
 
-class checksum:
-    def __init__(self, dir):
-        self.dir = dir
+def checksum (dir):
+    dir = os.path.abspath(dir)
+    md5 = hashlib.md5()
+    for root,dirs files in os.walk(dir):
+        for name in files:
+            filepath = os.path.join(root,names) # joins the path and the file.
+        try:
+          f1 = open(filepath, 'rb')
+        except:
+          # You can't open the file for some reason
+          f1.close()
+          continue
+    while 1:
+        buf = f1
+    
         
 #   SHAhash = hashlib.md5()
 #   for root, dirs, files in os.walk(dir):

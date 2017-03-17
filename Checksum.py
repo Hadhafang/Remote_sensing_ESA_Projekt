26a134fedcb9f291869606f0aad62c6184d05aab
Hadhafang <ala09lbe@student.lu.se>
2017-03-17 10:37:50 +0100

    Made Checsum into a class
    Will produce MD5 of root folder

--- Checksum.py
+++ Checksum.py
@@ -7,18 +7,19 @@ Created on Thu Mar 16 12:04:52 2017
 """
 
 
-
-def checksum(dir):
-
-   SHAhash = hashlib.md5()
-   for root, dirs, files in os.walk(dir):
-      for names in files:
-          filepath = os.path.join(root,names)
-   while 1:
-      # Read file in as little chunks
-      buf = f1.read(4096)
-      if not buf : break
-      SHAhash.update(hashlib.md5(buf).hexdigest())
-
-      
-checksum(os.path.abspath('C:\Logg\33UVB\S2A_MSIL1C_20161208T102422_N0204_R065_T33UVB_20161208T102418.SAFE')
+class checksum:
+    def __init__(self, dir):
+        self.dir = dir
+        
+#   SHAhash = hashlib.md5()
+#   for root, dirs, files in os.walk(dir):
+#      for names in files:
+#          filepath = os.path.join(root,names)
+#   while 1:
+#      # Read file in as little chunks
+#      buf = f1.read(4096)
+#      if not buf : break
+#      SHAhash.update(hashlib.md5(buf).hexdigest())
+#
+#      
+#checksum(os.path.abspath('C:\Logg\33UVB\S2A_MSIL1C_20161208T102422_N0204_R065_T33UVB_20161208T102418.SAFE')

345a78f85c851499635e18567277ae046204efe2
Hadhafang <ala09lbe@student.lu.se>
2017-03-19 15:24:51 +0100

    Återställde det hela

--- Checksum.py
+++ Checksum.py
@@ -1,17 +1,40 @@
-# -*- coding: utf-8 -*-
 import os, hashlib
+import os
 """
 Created on Thu Mar 16 12:04:52 2017
 
-@author: Hadhafang
+@@ -7,32 +7,45 @@ Created on Thu Mar 16 12:04:52 2017
 Will produce MD5 of root folder
 """
 
+from iterateMapp import walklevel
+
+
+        
 
 def checksum (dir):
     dir = os.path.abspath(dir)
     md5 = hashlib.md5()
     for root,dirs files in os.walk(dir):
+# -------------------------------------------------------------
+# A HELP FUNKTION THAT MAKES THE ACTUALL HASH
+# -------------------------------------------------------------
+    
+    def hashMaker(dir, file):
+        import hashlib
+        os.chdir(dir)
+        hasher = hashlib.md5()
+        with open(file, 'rb') as afile:
+            buf = afile.read()
+            hasher.update(buf)
+            return hasher.hexdigest()
+# -------------------------------------------------------------
+# ACTUALL CHECKSUM CODE
+# -------------------------------------------------------------    
+    directory = os.path.abspath(dir)
+    
+    hashMatrix= [['Inspire',0],['Manifest',0],['Mtd',0]]
+    for root, dirs, files in walklevel(directory, 0):
         for name in files:
             filepath = os.path.join(root,names) # joins the path and the file.
         try:
@@ -22,17 +45,19 @@ def checksum (dir):
           continue
     while 1:
         buf = f1
+    #These three should be the only files in files but the checking is for safty
+            if name.endswith('INSPIRE.xml'):
+                hashMatrix [0][1] = hashMaker(root,name)  
+            elif name.endswith('manifest.safe'):
+                hashMatrix [1][1]  = hashMaker(root,name)
+            elif name.endswith('.xml'):
+                if "MTD" in name:
+                   hashMatrix [2][1]  = hashMaker(root, name)
+            
+            
+                    
+    return hashMatrix
     
-        
-#   SHAhash = hashlib.md5()
-#   for root, dirs, files in os.walk(dir):
-#      for names in files:
-#          filepath = os.path.join(root,names)
-#   while 1:
-#      # Read file in as little chunks
-#      buf = f1.read(4096)
-#      if not buf : break
-#      SHAhash.update(hashlib.md5(buf).hexdigest())
-#
-#      
-#checksum(os.path.abspath('C:\Logg\33UVB\S2A_MSIL1C_20161208T102422_N0204_R065_T33UVB_20161208T102418.SAFE')
+                
+path = os.path.abspath(r'c:\logg\33uvb\s2a_msil1c_20161208t102422_n0204_r065_t33uvb_20161208t102418.safe')
+print checksum(path)    

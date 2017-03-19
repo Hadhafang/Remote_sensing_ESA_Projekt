faf949e60bcc8c6f39f824b340cd5d2a0e7597e0
Hadhafang <ala09lbe@student.lu.se>
2017-03-19 15:21:01 +0100

    Revert "Fungerande Checksum"
    This reverts commit 495444a713ccc0b76c4ffb62b842411e08677ea9.

--- iterateMapp.py
+++ iterateMapp.py
@@ -5,21 +5,20 @@ import xlsxwriter
 '''
 @author: manand83
 '''
+
 # -------------------------------------------------------------
 # METHOD THAT DEFINES HOW DEEP THE ITERATION WILL GO, IF LEVEL
 # IS SET TO 2 THE ITERATION WILL GO 2 FOLDERS DEEP AND NOT FURHTER
-# From: https://stackoverflow.com/questions/229186/os-walk-without-digging-into-directories-below/234329#234329
 # -------------------------------------------------------------
 def walklevel(some_dir, level):
     some_dir = some_dir.rstrip(os.path.sep)
-    assert os.path.isdir(some_dir) #Is it really a directory? IF not AssertionError
+    assert os.path.isdir(some_dir)
     num_sep = some_dir.count(os.path.sep)
     for root, dirs, files in os.walk(some_dir):
         yield root, dirs, files
         num_sep_this = root.count(os.path.sep)
         if num_sep + level <= num_sep_this:
             del dirs[:]
-
 # -------------------------------------------------------------
 # DECLARE VECTORS TO STORE METADATA
 # -------------------------------------------------------------

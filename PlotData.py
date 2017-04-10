769421d59120501f920c89a21754e7872d78eed4
MikaelRyd <mikaelrydstrom@hotmail.com>
2017-04-10 17:20:21 +0200

    updated plotData

--- PlotData.py
+++ PlotData.py
@@ -9,48 +9,58 @@ import matplotlib.pyplot as plt
 Uses the excel-document to plot the different cloudiness values of a tile. X-axis is based on start time, and y-axis dictates how cloudy the tile is.
 '''
 
-def plot(dir,filename):
+def plotData(dir,filename):
    os.chdir(dir)
    wb = xlrd.open_workbook(filename)
    first_sheet = wb.sheet_by_index(0)
    xticks = []
    x = []
    y = []
-   counter = 0
+   counter = 0 # ugly way to test if tile-name actually exists. it works though
+   
+   
    name = raw_input('Please enter sought tilename, e.g. 28PDC' +'\n')
    limit = raw_input('Would you like to set a cloud factor limit? y/n' +'\n')
+   
+   
    if(limit == 'y'):
-       lim = input('Set a cloud limit, for example 50. 0 is total lack of clouds and 100 is full cloudiness.' + '\n' )
+       lim = input('Set a cloud limit, for example 50.' + '\n' + '0 is total lack of clouds and 100 is full cloudiness.' + '\n' )
        for row in range(first_sheet.nrows):
-           if name == first_sheet.cell_value(row,0):
+           if name == first_sheet.cell_value(row,0): 
                if(int(float(first_sheet.cell_value(row,2)))<lim):
                    x.append(row)
                    xticks.append([first_sheet.cell_value(row,3)])
                    y.append(first_sheet.cell_value(row,2))
-               counter+=1
+                   counter+=1
    else:
        for row in range(first_sheet.nrows):
            if name == first_sheet.cell_value(row,0):
                x.append(row)
                xticks.append([first_sheet.cell_value(row,3)])
                y.append(first_sheet.cell_value(row,2))
-           counter+=1
-   plt.title(name)   
-   plt.tick_params(axis='both', which='major', labelsize=13)
-   plt.tick_params(axis='both', which='minor', labelsize=10)
-   plt.ylim(0,100,10)      
-   plt.xlabel('Start time',fontsize =14)
-   plt.ylabel('Cloudiness',fontsize =14)
-   plt.xticks(x,xticks,rotation=15)
-   plt.bar(x,y)        
-   plt.show()
+               counter+=1
+           
+# -------------------------------------------------------------
+# Plots the cloud data with the help of matplotlib. Plots the data as a bar plot
+# -------------------------------------------------------------        
    
    if counter == 0:   
        print('There is no such tilename')
+   else:
+       plt.title(name)   
+       plt.tick_params(axis='both', which='major', labelsize=13)
+       plt.tick_params(axis='both', which='minor', labelsize=10)
+       plt.ylim(0,100,10)       
+       plt.xlabel('Start time',fontsize =14)
+       plt.ylabel('Cloudiness',fontsize =14)    
+       plt.xticks(x,xticks,rotation=15)
+       plt.bar(x,y)        
+       plt.show()
+   
        
 
 
 
-plot('C:\Users\Mikael\Downloads','sentinel123.xlsx')
+plotData('C:\Users\Mikael\Downloads','sentinel123.xlsx')
 
        

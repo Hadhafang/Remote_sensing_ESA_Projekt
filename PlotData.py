953a3c73ad10370fe0f9b527177860caaa0eba21
MikaelRyd <mikaelrydstrom@hotmail.com>
2017-04-18 16:17:14 +0200

    Fixed PlotData
    Converted plot to a horizontal barplot, and fixed the ticks. Also
    trimmed start-time, so now only date and hour shows.

--- PlotData.py
+++ PlotData.py
@@ -13,7 +13,7 @@ def plotData(dir,filename):
    os.chdir(dir)
    wb = xlrd.open_workbook(filename)
    first_sheet = wb.sheet_by_index(0)
-   xticks = []
+   yticks = []
    x = []
    y = []
    counter = 0 # ugly way to test if tile-name actually exists. it works though
@@ -28,16 +28,20 @@ def plotData(dir,filename):
        for row in range(first_sheet.nrows):
            if name == first_sheet.cell_value(row,0): 
                if(int(float(first_sheet.cell_value(row,2)))<lim):
-                   x.append(row)
-                   xticks.append([first_sheet.cell_value(row,3)])
-                   y.append(first_sheet.cell_value(row,2))
+                   y.append(row)
+                   start_time = first_sheet.cell_value(row,3)
+                   start_time = start_time[:-11]
+                   yticks.append(start_time)
+                   x.append(first_sheet.cell_value(row,2))
                    counter+=1
    else:
        for row in range(first_sheet.nrows):
            if name == first_sheet.cell_value(row,0):
-               x.append(row)
-               xticks.append([first_sheet.cell_value(row,3)])
-               y.append(first_sheet.cell_value(row,2))
+               y.append(row)
+               start_time = first_sheet.cell_value(row,3)
+               start_time = start_time[:-11]
+               yticks.append(start_time)
+               x.append(first_sheet.cell_value(row,2))
                counter+=1
            
 # -------------------------------------------------------------
@@ -50,11 +54,11 @@ def plotData(dir,filename):
        plt.title(name)   
        plt.tick_params(axis='both', which='major', labelsize=13)
        plt.tick_params(axis='both', which='minor', labelsize=10)
-       plt.ylim(0,100,10)       
-       plt.xlabel('Start time',fontsize =14)
-       plt.ylabel('Cloudiness',fontsize =14)    
-       plt.xticks(x,xticks,rotation=15)
-       plt.bar(x,y)        
+       plt.xlim(0,100,10)       
+       plt.ylabel('Start time',fontsize =14)
+       plt.xlabel('Cloudiness',fontsize =14)    
+       plt.yticks(y,yticks)
+       plt.barh(y,x,align ='center')        
        plt.show()
    
        
